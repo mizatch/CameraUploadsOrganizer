d6250bc82cee9c6572b978153c0c175ace294a60
Mitch Lake <mitch@gilliongroup.com>
2017-04-23 19:42:27 -0500

    created form to organize

--- CameraUploadsOrganizer/Form1.Designer.cs
+++ CameraUploadsOrganizer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CameraUploadsOrganizer
 {
-    partial class Form1
+    partial class Organizer
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.fbdUploadsFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenUploadsDialog = new System.Windows.Forms.Button();
+            this.lblCameraUploadsLocation = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fbdPhotosFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(489, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Camera Uploads Folder Location";
+            // 
+            // btnOpenUploadsDialog
+            // 
+            this.btnOpenUploadsDialog.Location = new System.Drawing.Point(698, 70);
+            this.btnOpenUploadsDialog.Name = "btnOpenUploadsDialog";
+            this.btnOpenUploadsDialog.Size = new System.Drawing.Size(304, 74);
+            this.btnOpenUploadsDialog.TabIndex = 1;
+            this.btnOpenUploadsDialog.Text = "Select Folder";
+            this.btnOpenUploadsDialog.UseVisualStyleBackColor = true;
+            this.btnOpenUploadsDialog.Click += new System.EventHandler(this.btnOpenUploadsDialog_Click);
+            // 
+            // lblCameraUploadsLocation
+            // 
+            this.lblCameraUploadsLocation.AutoSize = true;
+            this.lblCameraUploadsLocation.Location = new System.Drawing.Point(96, 175);
+            this.lblCameraUploadsLocation.Name = "lblCameraUploadsLocation";
+            this.lblCameraUploadsLocation.Size = new System.Drawing.Size(0, 37);
+            this.lblCameraUploadsLocation.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(698, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(304, 76);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Organize Photos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(217, 37);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Photos Folder";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(698, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(304, 74);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Select Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Organizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1103, 401);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblCameraUploadsLocation);
+            this.Controls.Add(this.btnOpenUploadsDialog);
+            this.Controls.Add(this.label1);
+            this.Name = "Organizer";
+            this.Text = "Camera Uploads Organizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbdUploadsFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpenUploadsDialog;
+        private System.Windows.Forms.Label lblCameraUploadsLocation;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog fbdPhotosFolder;
     }
 }
 
--- CameraUploadsOrganizer/Form1.cs
+++ CameraUploadsOrganizer/Form1.cs
@@ -1,20 +1,196 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace CameraUploadsOrganizer
 {
-    public partial class Form1 : Form
+    public partial class Organizer : Form
     {
-        public Form1()
+        private static readonly Regex R = new Regex(":");
+
+        private readonly List<string> _ignoredFiles = new List<string>
+        {
+            ".dropbox"
+        };
+
+        private readonly List<string> _ignoredExtensions = new List<string>
+        {
+            ".ini"
+        };
+
+        public Organizer()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpenUploadsDialog_Click(object sender, EventArgs e)
+        {
+            fbdUploadsFolder.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var photosFolder = fbdPhotosFolder.SelectedPath;
+
+            var cameraUploadsPath = fbdUploadsFolder.SelectedPath;
+
+            var archivePath = Path.Combine(cameraUploadsPath, "archive");
+
+            var files = Directory.GetFiles(cameraUploadsPath);
+
+            foreach (var originalFilePath in files)
+            {
+                if (_ignoredFiles.Any(a => originalFilePath.EndsWith(a)))
+                {
+                    continue;
+                }
+
+                var extension = Path.GetExtension(originalFilePath);
+
+                if (_ignoredExtensions.Contains(extension))
+                {
+                    continue;
+                }
+
+                var fileName = Path.GetFileName(originalFilePath);
+
+                var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(originalFilePath);
+
+                DateTime dateTaken;
+
+                if (!IsFileAMovie(originalFilePath) && CanGetDateFromImage(originalFilePath))
+                {
+                    dateTaken = GetDateTakenFromImage(Path.Combine(cameraUploadsPath, originalFilePath));
+                }
+                else if (CanGetDateFromFileName(fileNameWithoutExtension))
+                {
+                    dateTaken = GetDateFromFileName(fileNameWithoutExtension);
+                }
+                else if (CanGetDateFromUnderscoredFileName(fileNameWithoutExtension))
+                {
+                    dateTaken = GetDateFromUnderscoredFileName(fileNameWithoutExtension);
+                }
+                else
+                {
+                    continue;
+                }
+
+                var newFolderName = dateTaken.ToString("yyyy-MM-dd");
+
+                if (!Directory.Exists(Path.Combine(photosFolder, newFolderName)))
+                {
+                    Directory.CreateDirectory(Path.Combine(photosFolder, newFolderName));
+                }
+
+                var newFilePath = Path.Combine(photosFolder, newFolderName, fileName);
+
+
+                if (!File.Exists(newFilePath))
+                {
+                    File.Copy(originalFilePath, newFilePath, false);
+                }
+
+                File.Move(originalFilePath, Path.Combine(archivePath, fileName));
+
+            }
+
+        }
+
+        private bool IsFileAMovie(string filePath)
+        {
+            var extension = Path.GetExtension(filePath);
+
+            return extension == ".mov" || extension == ".mp4";
+        }
+
+        // 2011-02-11 19.22.56
+        // IMG_20160529_184017
+        private readonly string[] _dateTimeFormats =
+        {
+            "yyyy-MM-dd HH.mm.ss",
+            "yyyyMMdd"
+        };
+
+        public bool CanGetDateFromFileName(string fileName)
+        {
+            DateTime date;
+            return DateTime.TryParseExact(fileName, _dateTimeFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out date);
+        }
+
+        public DateTime GetDateFromFileName(string fileName)
+        {
+            DateTime date;
+            DateTime.TryParseExact(fileName, _dateTimeFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out date);
+            return date;
+        }
+
+        public bool CanGetDateFromUnderscoredFileName(string fileName)
+        {
+            var strings = fileName.Split('_');
+
+            if (strings.Length < 2)
+            {
+                return false;
+            }
+
+            DateTime date;
+            return DateTime.TryParseExact(strings[1], _dateTimeFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out date);
+        }
+
+        public DateTime GetDateFromUnderscoredFileName(string fileName)
+        {
+            var strings = fileName.Split('_');
+
+            DateTime date;
+            DateTime.TryParseExact(strings[1], _dateTimeFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out date);
+            return date;
+        }
+
+        public static DateTime GetDateTakenFromVideo(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (var myImage = Image.FromStream(fs, false, false))
+            {
+                var propItem = myImage.GetPropertyItem(36867);
+                var dateTaken = R.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                return DateTime.Parse(dateTaken);
+            }
+        }
+
+        public static bool CanGetDateFromImage(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (var myImage = Image.FromStream(fs, false, false))
+            {
+                return myImage.PropertyIdList.Contains(36867);
+            }
+        }
+
+        public static DateTime GetDateTakenFromImage(string path)
+        {
+            using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (var image = Image.FromStream(fileStream, false, false))
+            {
+                var propertyItem = image.GetPropertyItem(36867);
+                var dateTaken = R.Replace(Encoding.UTF8.GetString(propertyItem.Value), "-", 2);
+                return DateTime.Parse(dateTaken);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fbdPhotosFolder.ShowDialog();
         }
     }
 }
